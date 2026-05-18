d4d16e983b1448098dd0cec7789fa9a95b8b651b
Farrukh Seyer Hasan <59789349+farrukh2002@users.noreply.github.com>
2026-05-18 12:49:10 +0530

    dataipa: gsi: Handle stuck STOP_IN_PROC channels gracefully
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- qcom/opensource/dataipa/drivers/platform/msm/gsi/gsi.c
+++ qcom/opensource/dataipa/drivers/platform/msm/gsi/gsi.c
@@ -3685,9 +3685,9 @@ int gsi_stop_channel(unsigned long chan_hdl)
 	}
 
 	if (ctx->state == GSI_CHAN_STATE_STOP_IN_PROC) {
-		GSIERR("chan=%lu busy try again\n", chan_hdl);
-		res = -GSI_STATUS_AGAIN;
-		goto free_lock;
+		dev_warn(gsi_ctx->dev, "chan=%lu stop not confirmed by HW, forcing stopped\n",
+			chan_hdl);
+		ctx->state = GSI_CHAN_STATE_STOPPED;
 	}
 
 	/* If channel is stopped succesfully and has an event with IRQ type MSI
